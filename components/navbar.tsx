97fb8e421df5871b09ab5b87f008e426b67f3583
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-11-30 22:24:04 +0400

    modified settings age

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,9 @@
 import { UserButton, auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation';
 
 import { MainNav } from '@/components/main-nav';
 import StoreSwitcher from '@/components/store-switcher';
-import { redirect } from 'next/navigation';
+
 import prismadb from '@/lib/prismadb';
 const Navbar = async () => {
     const { userId } = auth();

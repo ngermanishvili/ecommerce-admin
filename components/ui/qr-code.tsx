4c2c43dc09a1435a71a677166be0cd5cbe7a04ab
samxaradzenika <110243055+samxaradzenika@users.noreply.github.com>
2023-12-07 18:55:12 +0400

    s

--- components/ui/qr-code.tsx
+++ components/ui/qr-code.tsx
@@ -1,26 +1,26 @@
-"use client"
-import { useEffect, useRef } from 'react';
-import QRCode from 'qrcode';
+"use client";
+import {useEffect, useRef} from "react";
+import QRCode from "qrcode";
 
 interface QRCodeGeneratorProps {
-    text: string;
+  text: string;
 }
 
-const QRCodeGenerator: React.FC<QRCodeGeneratorProps> = ({ text }) => {
-    const qrCodeRef = useRef<HTMLCanvasElement>(null);
+const QRCodeGenerator: React.FC<QRCodeGeneratorProps> = ({text}) => {
+  const qrCodeRef = useRef<HTMLCanvasElement>(null);
 
-    useEffect(() => {
-        // Generate QR code when the component mounts
-        if (qrCodeRef.current) {
-            QRCode.toCanvas(qrCodeRef.current, text, (error) => {
-                if (error) {
-                    console.error(error);
-                }
-            });
+  useEffect(() => {
+    // Generate QR code when the component mounts
+    if (qrCodeRef.current) {
+      QRCode.toCanvas(qrCodeRef.current, text, (error) => {
+        if (error) {
+          console.error(error);
         }
-    }, [text]);
+      });
+    }
+  }, [text]);
 
-    return <canvas ref={qrCodeRef} />;
+  return <canvas ref={qrCodeRef} />;
 };
 
 export default QRCodeGenerator;

eb13c8c23fc25a5c84f9071937957dceb16a0664
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-12-11 15:52:57 +0400

    asd

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,38 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          {/* Add the Messenger chat plugin code here */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                var chatbox = document.createElement('div');
+                chatbox.id = 'fb-customer-chat';
+                chatbox.className = 'fb-customerchat';
+                document.body.appendChild(chatbox);
+
+                chatbox.setAttribute('page_id', 'YOUR_PAGE_ID'); // Replace with your Facebook Page ID
+                chatbox.setAttribute('attribution', 'biz_inbox');
+                
+                window.fbAsyncInit = function() {
+                  FB.init({
+                    xfbml: true,
+                    version: 'v18.0',
+                  });
+                };
+                
+                (function(d, s, id) {
+                  var js, fjs = d.getElementsByTagName(s)[0];
+                  if (d.getElementById(id)) return;
+                  js = d.createElement(s); js.id = id;
+                  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+                  fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));
+              `,
+            }}
+          />
+          {/* End of Messenger chat plugin code */}
+        </head>
         <body className={inter.className}>
           <ToasterProvider />
           <ModalProvider />
@@ -28,6 +60,5 @@ export default function RootLayout({
         </body>
       </html>
     </ClerkProvider>
-
   )
 }

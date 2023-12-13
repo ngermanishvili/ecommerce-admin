b34f8fccfbfa6eb56ca4f7d2d3f813de9381d27c
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-12-13 20:06:54 +0400

    add fb chat sdk

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { ModalProvider } from '@/providers/modal-provider'
 import { ToasterProvider } from '@/providers/toast-provider'
 import Script from 'next/script'
-import FacebookChatSDK from '@/components/frontend-main/components/facebook-chat'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -27,11 +26,33 @@ export default function RootLayout({
           <ToasterProvider />
           <ModalProvider />
           {children}
-          <div id="fb-customer-chat" className="fb-customerchat" />
-          <FacebookChatSDK />
+          <div id="fb-root"></div>
+          <div id="fb-customer-chat" className="fb-customerchat"></div>
+
+          <Script id="fb-chat" strategy="lazyOnload">
+            {`
+              var chatbox = document.getElementById('fb-customer-chat');
+              chatbox.setAttribute("page_id", "115850238226188");
+              chatbox.setAttribute("attribution", "biz_inbox");
+
+              window.fbAsyncInit = function() {
+                FB.init({
+                  xfbml: true,
+                  version: 'v18.0'
+                });
+              };
+
+              (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+                fjs.parentNode.insertBefore(js, fjs);
+              }(document, 'script', 'facebook-jssdk'));
+              `}
+          </Script>
         </body>
       </html>
     </ClerkProvider>
-
   )
 }
